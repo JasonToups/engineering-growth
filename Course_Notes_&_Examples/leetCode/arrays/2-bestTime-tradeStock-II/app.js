6a1bbf25ac565c622204c3642b9424bd46133af8
Jason Toups <me@jasontoups.com>
2022-03-30 11:10:59 -0700

    added new trade stock algo

--- Course_Notes_&_Examples/leetCode/arrays/2-bestTime-tradeStock-II/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/2-bestTime-tradeStock-II/app.js
@@ -28,8 +28,6 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 Constraints:
 1 <= prices.length <= 3 * 10 ^ 4
 0 <= prices[i] <= 10 ^ 4
-
-
 */
 
 /*
@@ -44,49 +42,30 @@ Loop through the array.
 If the next index is lower than the current index & you have stock, sell the stock.
   Calculate the profit & store it in the profit variable.
 If the next index is higher than the current index, buy at the current index rate.
-
+The index of the loop should compare to the previous index.
+If the previous index is lower than the current, then add the difference to the max variable.
+If the previous index is higher than the current, then don't add anything to the max variable.
 */
 
-// var maxProfit = function (prices) {
-//   console.log(prices)
-//   let i = 0;
-//   let valley = prices[0];
-//   let peak = prices[0];
-//   let profit = 0;
-//   while (i < prices.length - 1) {
-//     while (i < prices.length - 1 && prices[i] >= prices[i + 1])
-//       i++;
-//     valley = prices[i];
-//     while (i < prices.length - 1 && prices[i] <= prices[i + 1])
-//       i++;
-//     peak = prices[i];
-//     profit += peak - valley;
-//   }
-//   return profit;
-// };
-var maxProfit = function (prices) {
-  console.log(prices)
-  let i = 0;
-  let valley = prices[0];
-  let peak = prices[0];
-  let profit = 0;
-  while (i < prices.length - 1) {
-    while (prices[i] >= prices[i + 1])
-      i++;
-    valley = prices[i];
-    while (prices[i] <= prices[i + 1])
-      i++;
-    peak = prices[i];
-    profit += peak - valley;
+function maxProfit(prices) {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    console.log(prices)
+    console.log(prices[i])
+    console.log(prices[i - 1])
+      if (prices[i] > prices[i - 1]) {
+          maxProfit += prices[i] - prices[i - 1];
+          console.log(maxProfit)
+      }
   }
-  return profit;
-};
+  return maxProfit;
+}
 
-let array1 = [7, 1, 5, 3, 6, 4]
-console.log(maxProfit(array1));
+// let array1 = [7, 1, 5, 3, 6, 4]
+// console.log(maxProfit(array1));
 let array2 = [1, 2, 3, 4, 5]
 console.log(maxProfit(array2));
-let array3 = [7, 6, 4, 3, 1]
-console.log(maxProfit(array3));
-let array4 = [2, 1, 2, 0, 1]
-console.log(maxProfit(array4));
+// let array3 = [7, 6, 4, 3, 1]
+// console.log(maxProfit(array3));
+// let array4 = [2, 1, 2, 0, 1]
+// console.log(maxProfit(array4));

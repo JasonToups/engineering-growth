008b61bba0dcf29fed7124a07b5d0e5ae118b477
Jason Toups <me@jasontoups.com>
2022-04-05 11:42:36 -0700

    working through rotate array

--- Course_Notes_&_Examples/leetCode/arrays/3-rotateArray copy/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/3-rotateArray copy/app.js
@@ -43,16 +43,26 @@ Constraints:
 /* MUTATE NUMS */
 
 var rotate = function(nums, k) {
-    console.log(nums);
-    
+  let numsSplice = nums.splice(nums.length - k, k);
+  // console.log(numsSplice);
+  for (let i = numsSplice.length - 1; i >= 0 ; i--) {
+    nums.unshift(numsSplice[i]);
+  }
+  return nums;
 };
+// Optimize
+// placing each element in its original position while keeping track of the element originally in that position. Basically, at every step, we place an element in its rightful position and keep track of the element already there or the one being overwritten in an additional variable.
 
 // Inputs
 var nums = [1, 2, 3, 4, 5, 6, 7];
 var k = 3;
 console.log(rotate(nums, k));
-// Output: [5, 6, 7, 1, 2, 3, 4];
-var nums = [-1, -100, 3, 99];
+console.log("Should be: [ 5, 6, 7, 1, 2, 3, 4]");
+var nums =  [-1, -100, 3, 99];
 var k = 2;
 console.log(rotate(nums, k));
-// Output: [5, 6, 7, 1, 2, 3, 4];
+console.log("Should be: [ 3, 99, -1, -100]");
+var nums = [1,2];
+var k = 5;
+console.log(rotate(nums, k));
+console.log("Should be: [ 2, 1]");

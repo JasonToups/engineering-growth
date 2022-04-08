eb661f375d46f54666f3b54e8206672169339a70
Jason Toups <me@jasontoups.com>
2022-04-08 10:13:11 -0700

    adds solution for 5 single number

--- Course_Notes_&_Examples/leetCode/arrays/5-singleNumber/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/5-singleNumber/app.js
@@ -3,9 +3,7 @@ PROBLEM
 
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
-Note:
-
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 Example 1:
 
@@ -20,19 +18,31 @@ Output: 4
  */
 
 /*
- * @param {number[]} nums
- * @return {number}
+Constraints:
+
+  1 <= nums.length <= 3 * 104
+  -3 * 104 <= nums[i] <= 3 * 104
+  Each element in the array appears twice except for one element which appears only once.
+
  */
+
+/* Solution Plan
+
+I could create an object to log each time a number is encountered, as a unique property, and then add 1 to the value.
+
+Then after going through the nums array, look for the number that only has the value of 1.
+ */
+
 var singleNumber = function (nums) {
   let map = {};
-  for (let num of nums) {
-    if (map[num]) map[num]++;
-    else map[num] = 1;
+  
+  for (num in nums){
+    map[nums[num]] = map[nums[num]] + 1 || 1;
   }
-  for (let num of nums) {
-    if (map[num] === 1) return num;
+
+  for (key in map) {
+    if (map[key] === 1) return key
   }
-  return map;
 };
 
 console.log(singleNumber([2, 2, 1]));

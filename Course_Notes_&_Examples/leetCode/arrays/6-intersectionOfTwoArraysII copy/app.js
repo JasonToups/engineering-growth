90b667662dae7632b82ebc19a04f5130f8703f81
Jason Toups <me@jasontoups.com>
2022-04-09 10:27:03 -0700

    working through solution of intersection of 2 arrays

--- Course_Notes_&_Examples/leetCode/arrays/6-intersectionOfTwoArraysII copy/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/6-intersectionOfTwoArraysII copy/app.js
@@ -20,124 +20,52 @@ Follow up:
  * @return {number[]}
  */
 
-var intersect = function (nums1, nums2) {
-  var store, array;
-  var number = [];
-  if (nums1.length > nums2.length) {
-    store = nums1;
-    array = nums2;
-  } else {
-    store = nums2;
-    array = nums1;
-  }
+var intersect = function (nums1, nums2) 
+{
+  nums1 = nums1.sort();
+  nums2 = nums2.sort();
+  console.log(nums1);
+  console.log(nums2);
+  let pointer1 = 0, pointer2 = 0;
+  let intersect = [];
 
-  for (var i = 0; i < array.length; i++) {
-    var v = array[i];
-    if (store.indexOf(v) >= 0) {
-      store[store.indexOf(v)] = null;
-      number.push(v);
+  while (pointer1 < nums1.length && pointer2 < nums2.length) 
+  {
+    if (nums1[pointer1] < nums2[pointer2]) { nums1++;} 
+    else if (nums1[pointer1] > nums2[pointer2]) {nums2++;} 
+    else 
+    {
+      intersect.push(nums1[pointer1]);
+      pointer1++;
+      pointer2++;
     }
   }
-  return number;
-};
+  return intersect;
+}
 
 // Example 1:
 var nums1 = [1, 2, 2, 1];
 var nums2 = [2, 2];
 console.log(intersect(nums1, nums2));
-// Output: [2, 2]
+console.log("Output: [2, 2]");
 
 // Example 2:
 
 var nums1 = [4, 9, 5];
 var nums2 = [9, 4, 9, 8, 4];
 console.log(intersect(nums1, nums2));
-// Output: [4, 9];
+console.log("Output: [4, 9]");
 
 // Example 3:
 
 var nums1 = [2, 1];
 var nums2 = [1, 2];
 console.log(intersect(nums1, nums2));
-// Output: [1, 2];
+console.log("Output: [1, 2]");
 
-// FAILING TEST CASE
 // Example 4:
-var nums1 = [
-  43,
-  85,
-  49,
-  2,
-  83,
-  2,
-  39,
-  99,
-  15,
-  70,
-  39,
-  27,
-  71,
-  3,
-  88,
-  5,
-  19,
-  5,
-  68,
-  34,
-  7,
-  41,
-  84,
-  2,
-  13,
-  85,
-  12,
-  54,
-  7,
-  9,
-  13,
-  19,
-  92,
-];
-var nums2 = [
-  10,
-  8,
-  53,
-  63,
-  58,
-  83,
-  26,
-  10,
-  58,
-  3,
-  61,
-  56,
-  55,
-  38,
-  81,
-  29,
-  69,
-  55,
-  86,
-  23,
-  91,
-  44,
-  9,
-  98,
-  41,
-  48,
-  41,
-  16,
-  42,
-  72,
-  6,
-  4,
-  2,
-  81,
-  42,
-  84,
-  4,
-  13,
-];
+var nums1 = [43,85,49,2,83,2,39,99,15,70,39,27,71,3,88,5,19,5,68,34,7,41,84,2,13,85,12,54,7,9,13,19,92,];
+var nums2 = [10,8,53,63,58,83,26,10,58,3,61,56,55,38,81,29,69,55,86,23,91,44,9,98,41,48,41,16,42,72,6,4,2,81,42,84,4,13,];
 
 console.log(intersect(nums1, nums2));
 // Expected Output [2,83,3,41,84,13,9]

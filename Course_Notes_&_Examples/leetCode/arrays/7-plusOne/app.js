83fb58e7360dac5cfb1d8bf24e2b497dc35a7b1b
Jason Toups <me@jasontoups.com>
2022-04-14 15:12:49 -0700

    debugging plusOne

--- Course_Notes_&_Examples/leetCode/arrays/7-plusOne/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/7-plusOne/app.js
@@ -21,16 +21,77 @@ convert the string to an array
 
 ISSUE - parseInt
 ParseInt is not parsing the full integer, and is stopping at around index 16
+
+FIX
+I don't think I can use the parseInt approach, because of the decimal restriction, so I should loop through the back of the array, and set logic to add one to the end.
+If the current index number is less than 9, then add one & return the num aray.
+If the current index number is 9, then set the current index to 0, and add 1 to the previous index.
+
+If the previous index is 9, then set the current index to 0, and keep checking previous indexes until the current index is not 9.
+
+Create a conditional for if the number at the 0 index is 9, then unshift a 1 to the front of the array.
+
  */
 
 var plusOne = function (digits) {
-
-  digits = (parseInt((digits.join('')) + 1)).toString().split('');
-  // console.log(typeof(digits));
-  return digits;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9 && i > 0) {
+      digits[i]++;
+      return digits;
+    } else if (i === 0 && digits[0] === 9) {
+      console.log('unshifting')
+      digits[0] = 0;
+      digits.unshift(1);
+      return digits;
+    } else if (digits[i] === 9 && i === 0) {
+      return digits;
+    } else {
+      digits[i] = 0;
+      if (digits[i - 1] < 9){
+        digits[i - 1]++;
+      }
+    }
+  }
 };
+// Debugging
+// var plusOne = function (digits) {
+//   for (let i = digits.length - 1; i >= 0; i--) {
+//     if (digits[i] < 9) {
+//       digits[i]++;
+//       return digits;
+//     } else {
+//       digits[i] = 0;
+//       if (i === 0 && digits[0] ===  0) {
+//         digits.unshift(1);
+//         return digits;
+//       } else {
+//         if (digits[i - 1] < 9){
+//           digits[i - 1]++;
+//         }
+//       }
+//     }
+//   }
+// };
 
 
+
+// Example 5:
+var digits = [8,9,9,9]
+console.log(digits);
+console.log(plusOne(digits));
+console.log("Should return Output: [9,0,0,0]")
+/* Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0]. */
+
+// Example 3:
+var digits = [9]
+console.log(digits);
+console.log(plusOne(digits));
+console.log("Should return Output: [1,0]")
+/* Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0]. */
 
 // Example 1:
 var digits = [1, 2, 3]
@@ -48,13 +109,6 @@ console.log("Should return Output: [4,3,2,2]")
 Incrementing by one gives 4321 + 1 = 4322.
 Thus, the result should be [4,3,2,2]. */
 
-// Example 3:
-var digits = [9]
-console.log(plusOne(digits));
-console.log("Should return Output: [1,0]")
-/* Explanation: The array represents the integer 9.
-Incrementing by one gives 9 + 1 = 10.
-Thus, the result should be [1,0]. */
 
 // Example 4:
 var digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
@@ -63,3 +117,4 @@ console.log("Should return Output: [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]")
 /* Explanation: The array represents the integer 9.
 Incrementing by one gives 9 + 1 = 10.
 Thus, the result should be [1,0]. */
+

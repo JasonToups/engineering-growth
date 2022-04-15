2a4467f26d971ffad384c02361be6de9e629c59d
Jason Toups <me@jasontoups.com>
2022-04-15 09:55:44 -0700

    updates answer to moveZeroes

--- Course_Notes_&_Examples/leetCode/arrays/8-moveZeroes/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/8-moveZeroes/app.js
@@ -21,19 +21,44 @@ Follow up: Could you minimize the total number of operations done?
 /* 
 Take the array and loop through it
   at each index of the loop we will check if the value is 0
-    If it is 0, then splice the current index and push it to the end of the array.
-    If it is not 0, then move to the next index.
+  set pushNum to the amount of elements to push
+    If it is 0 & the following index is zero, add one to pushNum.
+    If it is 0 & the following index is not zero, then splice the current index and push it to the end of the array.
+    nums.push(nums.splice(i, pushNum))
+    If it is not 0, then move to the next index & set pushNum to 1.
 
   return nums
     
 */
 
 var moveZeroes = function(nums) {
-  
+  let index = 0, pushNum = 1;
+  while (index < nums.length) {
+    if (nums[index] === 0 && nums[index + pushNum] === 0 ) {
+      pushNum++;
+    } else if (nums[index] === 0 && nums[index + pushNum] != 0 ) {
+      nums.push(...nums.splice(index, pushNum));
+      index++;
+      pushNum = 1;
+    } else {
+      index++
+      pushNum = 1;
+    }
+  }
   return nums
 };
 
-var num = [0,1,0,3,12]
+var num = [4,2,4,0,0,3,0,5,1,0]
+console.log(num)
+console.log(moveZeroes(num))
+console.log("Output: [4,2,4,3,5,1,0,0,0,0]")
+
+/* var num = [4,2,4,0,0,3,0,5,1,0]
+console.log(num)
+console.log(moveZeroes(num))
+console.log("Output: [4,2,4,3,5,1,0,0,0,0]") */
+
+/* var num = [0,1,0,3,12]
 console.log(num)
 console.log(moveZeroes(num))
 console.log("Output: [1,3,12,0,0]")
@@ -41,4 +66,4 @@ console.log("Output: [1,3,12,0,0]")
 var num = [0,0,0,3,12]
 console.log(num)
 console.log(moveZeroes(num))
-console.log("Output: [3,12,0,0, 0]")
+console.log("Output: [3,12,0,0, 0]") */

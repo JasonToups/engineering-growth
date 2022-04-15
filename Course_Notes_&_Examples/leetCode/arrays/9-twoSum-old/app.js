217b8299570b6d27a03557a7d56474e462c3be20
Jason Toups <me@jasontoups.com>
2022-04-15 09:57:56 -0700

    adds new challenge twoSum

--- Course_Notes_&_Examples/leetCode/arrays/9-twoSum-old/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/9-twoSum-old/app.js
@@ -40,7 +40,19 @@ Set a return variable to an empty array.
 // };
 // Single loop from other user submission.
 const twoSum = (nums, target) => {
- 
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return null;
 };
 
 // Example 1:

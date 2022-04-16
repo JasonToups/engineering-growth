2de9cfa5ced4264041be865e74e9c2e82a4f2381
Jason Toups <me@jasontoups.com>
2022-04-16 12:01:32 -0700

    sets up new sudoku challenge

--- Course_Notes_&_Examples/leetCode/arrays/10-validSudoku-old/app.js
+++ Course_Notes_&_Examples/leetCode/arrays/10-validSudoku-old/app.js
@@ -19,7 +19,69 @@ The given board size is always 9x9. */
  */
 
 const isValidSudoku = function (board) {
-  
+  for (let i = 0; i < 9; i++) {
+    if (!checkNums(board[i])) {
+      // console.log('not valid sudoku');
+      return false;
+    }
+  }
+  // checking columns
+  for (let i = 0; i < 9; i++) {
+    const column = [];
+    // at each column index of the board row, push the index of the next row to the column array
+    for (let j = 0; j < board[i].length; j++) {
+      column.push(board[j][i]);
+      // once you reach the end of the column, move to the next column index.
+    }
+    // validate the column array
+    if (!checkNums(column)) {
+      // console.log('not valid sudoku');
+      return false;
+    }
+  }
+
+  // checking quadrant
+  for (let x = 0; x < 9; x += 3) {
+    for (let y = 0; y < 9; y += 3) {
+      // console.log(x, y);
+      if (!checkNums(buildQuadrant(x, y, board))) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const buildQuadrant = (x, y, board) => {
+  let quadrant = [];
+  // let startingBlock = [x,y];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      quadrant.push(board[i + x][j + y]);
+    }
+  }
+  //WORKING
+  // for (let i = x; i < x + 3; i++) {
+  //   for (let j = y; j < y + 3; j++) {
+  //     quadrant.push(board[i][j]);
+  //   }
+  // }
+  console.log(quadrant);
+  return quadrant;
+};
+
+const checkNums = array => {
+  const sorted = [...array].sort();
+  for (let i = 0; i < sorted.length - 1; i++) {
+    if (sorted[i] != '.') {
+      sorted[i] = parseInt(sorted[i]);
+      // check next index for a greater value
+      if (sorted[i] >= sorted[i + 1]) {
+        return false;
+      }
+    }
+  }
   return true;
 };
 // Input:

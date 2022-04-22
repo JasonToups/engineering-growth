658d4b0ea5d8e52508ffc92f19a3f85868e021da
Jason Toups <me@jasontoups.com>
2022-04-22 15:19:56 -0700

    completes reverse-string

--- Course_Notes_&_Examples/leetCode/strings/1-reverse-string/app.js
+++ Course_Notes_&_Examples/leetCode/strings/1-reverse-string/app.js
@@ -13,24 +13,20 @@ You may assume all the characters consist of printable ascii characters.
   return s.reverse();
 }; */
 
+/* I could loop through the string array
+Starting with the second index, remove the index & unshift it to the start of the array.
+Do this as many times as the length of the array.
+I could do this as 1 operation with splice.
+s.splice(0, 1 , s[index])
+*/
+
 var reverseString = function(s) {
-  let start = 0;
-  let end = s.length - 1;
-  console.log(start)
-  console.log(end)
-  // loop through the array, and grab the value of the current index.
-  // Swap the values with the end of the array.
-  // Add one to start
-  // subtract one from end
-  // if start index is less than or equal to end index, return s.
-  while ( start <= end) {
-    let hold = s[start];
-    s[start] = s[end];
-    s[end] = hold;
-    start ++
-    end --
+  let index = 0;
+  while (index < s.length) {
+    s.unshift(...s.splice(index, 1));
+    index++
   }
-  return s
+  return s;
 };
 
 /* Most popular minimal time complexity
@@ -39,22 +35,20 @@ var reverseString = function(s) {
   @return {void} Do not return anything, modify s in-place instead.
   Unpack this later
  */
-var reverseString = function(s) {
-  let attempts = s.length % 2 ? (s.length - 1) / 2 :(s.length) / 2
-  
-  for (let i = 0; i < attempts; i++) {
-      [s[i], s[s.length - i -1]] = [s[s.length - i -1], s[i]]
-  }
-};
 
-*/
 
 // Example 1:
 var input = ["h","e","l","l","o"]
-// Output: ["o","l","l","e","h"]
-reverseString(input)
+var output = ["o","l","l","e","h"]
+console.log(`Function Return:`)
+console.log(reverseString(input))
+console.log(`Output:`)
+console.log(output)
 
 // Example 2:
-var input2 = ["H","a","n","n","a","h"]
-// Output: ["h","a","n","n","a","H"]
-reverseString(input2)
+// var input = ["H","a","n","n","a","h"]
+// var output = ["h","a","n","n","a","H"]
+// console.log(`Function Return:`)
+// console.log(reverseString(input))
+// console.log(`Output:`)
+// console.log(output)

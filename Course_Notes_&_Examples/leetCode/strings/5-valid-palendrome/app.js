700c5c424e8357eb305068d60ae2efef584474e5
Jason Toups <me@jasontoups.com>
2022-04-28 11:33:39 -0700

    completed valid-palendrome

--- Course_Notes_&_Examples/leetCode/strings/5-valid-palendrome/app.js
+++ Course_Notes_&_Examples/leetCode/strings/5-valid-palendrome/app.js
@@ -16,26 +16,89 @@ Constraints:
  * @return {boolean}
  **/
 
+/* 
+Input string
+Output boolean
+
+PLAN
+Done - Process the String Input
+Take the string input & remove all spaces & punctuation, then convert all letters to lowercase.
+Convert the string to an array
+
+Done - Check for Palendrome
+Loop through the processedArray.
+The index should only reach 1/2 of the length of the processedArray.
+At each loop, check if the processedArray[index] === processedArray[-index],
+if it doesn't then return false
+
+After the loop completes, then return true.
+
+ */
+
 var isPalindrome = function(s) {
-  var answer = true;
-  
-  return answer
+  let processedArray = [];
+  let index = 0;
+
+  function containsNumOrLetter(str) {
+    return /[0-90-9a-zA-Z]/.test(str);
+  }
+
+  function processString (string) {
+    index = 0;
+    processedArray = string.split("");
+    while (index < processedArray.length) {
+      if (containsNumOrLetter(processedArray[index])) {
+        processedArray[index] = processedArray[index].toLowerCase();
+      } else {
+        processedArray.splice(index, 1);
+        index--
+      }
+      index++
+    }
+  }
+
+  function checkPalendrome(array){
+    index = 0;
+    
+    while (index <= array.length / 2) {
+      if (array[index] !== array[array.length - index - 1]) {
+        console.log(`Positive Index ${index} ${array[index]}`)
+        console.log(`Negative Index ${array.length - index - 1} ${array[array.length - index - 1]}`)
+        return false
+      }
+      index++
+    }
+    return true
+  }
+
+  processString(s)
+
+  return checkPalendrome(processedArray)
 };
 
 // Example 1:
 
-var Input = "A man, a plan, a canal: Panama"
-console.log(isPalindrome(Input))
-console.log("Output: true")
+var input = "A man, a plan, a canal: Panama"
+// console.log(isPalindrome(input))
+// console.log("Output: true")
 
 // Example 2:
 
-var Input = "race a car"
-// console.log(isPalindrome(Input))
+var input = "race a car"
+// console.log(isPalindrome(input))
 // console.log("Output: false")
 
-var Input3 = "Marge, let's \"[went].\" I await {news} telegram."
-// console.log(isPalindrome(Input3)) 
+var input = "Marge, let's \"[went].\" I await {news} telegram."
+// console.log(isPalindrome(input)) 
+// console.log("Output: true")
 
+// TODO - debug this one
+var input = "0P"
+console.log(isPalindrome(input)) 
+console.log("Output: false")
+
+var input = "a"
+console.log(isPalindrome(input)) 
+console.log("Output: true")
 
 

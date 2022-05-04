1379c1eb0815240ebaec6d1417376697a7c1afee
Jason Toups <me@jasontoups.com>
2022-05-04 10:52:23 -0700

    starts project euler challenges

---
+++ interviews/Amazon/project-euler/1-multiples-of-3or5/app.js
@@ -0,0 +1,25 @@
+/* Multiples of 3 or 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+function multiplesThreeFive (number) {
+  let sum = 0;
+	return sum;
+};
+
+
+
+
+var n = 10
+console.log(multiplesThreeFive(n))
+// Output: "23"
+// Explanation: The sum of all multiples of 3 or 5 below 10 is 23
+
+
+var n = 1000
+// console.log(multiplesThreeFive(n))
+// Output: "?"

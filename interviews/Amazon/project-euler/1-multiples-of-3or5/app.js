9b7f4982b175f4e636b0a8657905c1c935686875
Jason Toups <me@jasontoups.com>
2022-05-04 11:50:46 -0700

    updates plan

--- interviews/Amazon/project-euler/1-multiples-of-3or5/app.js
+++ interviews/Amazon/project-euler/1-multiples-of-3or5/app.js
@@ -3,7 +3,16 @@
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
+*/
 
+/* 
+PLAN
+
+input number
+output number {sum}
+
+Use the modulus operator to check all of the numbers from 1 - input to see if the number is divisible by 3 or 5, then add it to the sum.
+  If the number is divisible by both, only add it once to the sum.
 */
 
 function multiplesThreeFive (number) {

b0f8c0046f0bb2576bcd16f066f0bb0def028fa6
Jason Toups <me@jasontoups.com>
2022-05-26 07:46:56 -0700

    updates longest common prefix

--- daily-coding-practice/leetCode/strings/9-longest-common-prefix/app.js
+++ daily-coding-practice/leetCode/strings/9-longest-common-prefix/app.js
@@ -13,43 +13,7 @@ Constraints:
  */
 
 var longestCommonPrefix = function(strs) {
-  // if you enter nothing, then you receive an empty string
-  if (!strs[0]) {
-    return ""
-    // if you enter a single word in an array, you will receive the same word
-  } else if (strs.length === 1) {
-    return strs[0]
-  }
-
-  let prefix = "";
-  let stringIndex = 0;
-  let currentLetter = "";
-
-  currentLetter = strs[0][0]
-
-  for (let arrayIndex = 0; arrayIndex < strs.length; arrayIndex++) {
-    if (strs[arrayIndex][stringIndex] === currentLetter) {
-      // if the length of the prefix is equal to the length of the first word in the array, there are no more matches to check, so return the prefix.
-      if (prefix.length === strs[0].length) {
-        return prefix
-      }
-      // at the end position of the array, if all letters at the same index are equal, do the following;
-      // TODO Optimization - this could be moved outside of the loop, and remove the conditional.
-      else if (arrayIndex === strs.length - 1) {
-        // append the currentLetter to prefix
-        prefix += currentLetter;
-        // loop back to the start of the array, by setting the arrayIndex to 0
-        arrayIndex = 0
-        // and add 1 to the stringIndex pointer.
-        stringIndex ++;
-        // set the currentLetter to the updated arrayIndex and stringIndex.
-        currentLetter = strs[arrayIndex][stringIndex]
-      }
-    } else {
-      // if one of the letters in the current loop isn't the same as the current letter, return the prefix
-      return prefix;
-    }
-  }
+  
 };
 
 //Example 1:

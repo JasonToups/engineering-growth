3b3687caa8955b44adef2985463a32c77dab5c0e
Jason Toups <me@jasontoups.com>
2022-05-27 09:38:43 -0700

    wrote plan for longest common prefix

--- daily-coding-practice/leetCode/strings/9-longest-common-prefix/app.js
+++ daily-coding-practice/leetCode/strings/9-longest-common-prefix/app.js
@@ -10,29 +10,77 @@ Constraints:
     0 <= strs[i].length <= 200
     strs[i] consists of only lower-case English letters.
 
- */
+*/
+
+/* 
+Inputs:
+Array of Strings, of various sizes
+
+Outputs:
+String - longest common prefix from the array of strings
+
+PLAN:
+We don't need to split the strings into arrays, we can just compare the first letter of each string, using bracket notation.
+
+The challenge will be the break condition of the loop. 
+Since we need to loop back to the beginning of the array only if all of the letters in the current index matches.
+
+We should use a while loop with two pointers.
+
+Pointers:
+1. arrayIndex - The first pointer will be the current index of the array of strings.
+2. stringIndex - The second pointer will be the current index of the current string.
+
+Return Variable:
+1. returnString - The return variable will be the longest common prefix.
+
+Loop: 
+Loop through the array of strings.
+At each index, check the stringIndex character of the arrayIndex string & compare that character to the next arrayIndex string at the same stringIndex.
+  If the characters match, then move onto the next string.
+  If the characters don't match, then break the loop.
+  If the final string index is reached, then we have a start of the common prefix.
+    We should save that character to the return string, by setting the return string to a substring of the current string by passing in (0, stringIndex).
+    And we should reset the arrayIndex to 0;
+    And increment the stringIndex by 1.
+
+The end condition of the loop is stringIndex < array[0].length.
+
+*/
 
 var longestCommonPrefix = function(strs) {
+  // Check for immediate negative conditions
+  if (strs.length === 0) {
+    return '';
+  }
+
+  // Setup return string
+  let returnString = '';
+  // Setup pointers
+  let strsIndex = 0;
+  let returnIndex = 0;
+
   
 };
 
 //Example 1:
 
-let strs1 = ["flower","flow","flight"]
-console.log(longestCommonPrefix(strs1))
-// Output: "fl"
-
+var strs = ["flower","flow","flight"]
+console.log(strs);
+console.log("Output: fl");
+console.log(longestCommonPrefix(strs));
 
 // Example 2:
-
-let strs2 = ["dog","racecar","car"]
-console.log(longestCommonPrefix(strs2))
-// Output: ""
+strs = ["dog","racecar","car"]
+console.log(strs);
+console.log('Output: ""');
+console.log(longestCommonPrefix(strs))
 // Explanation: There is no common prefix among the input strings.
 
 // Example 3;
-
-let strs3 = ["a"]
-console.log(longestCommonPrefix(strs3))
+strs = ["a"]
+console.log(strs);
+console.log('Output: a');
+console.log(longestCommonPrefix(strs))
 // Output: "a"
 
